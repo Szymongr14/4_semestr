10b7f6a21bfb8bea9da8b23e34331fdaa5c0fef9
Szymon Groszkowski <szymongr14@gmail.com>
2024-03-02 16:02:36 +0100

    done rozproszone_lab_1 without deleting

--- Rozproszone_lab/Rozproszone_lab1/hash_table.c
+++ Rozproszone_lab/Rozproszone_lab1/hash_table.c
@@ -6,34 +6,45 @@
 void add_to_value_node(void* value, HashNode* temp_node){
     ValueNode* temp_value_node = temp_node->value_head;
     while(temp_value_node->next != NULL){
-        temp_value_node = temp_value_node->next;
+        temp_value_node = (ValueNode *) temp_value_node->next;
     }
     ValueNode* new_value_node = malloc((sizeof(ValueNode)));
-    temp_value_node->next = new_value_node;
+    temp_value_node->next = (struct ValueNode *) new_value_node;
     new_value_node->value = value;
     new_value_node->next = NULL;
 }
 
 
-void add_new_hash_node_in_the_middle(HashNode* temp_node, void* value) {
+void add_new_hash_node_in_the_middle(HashNode* temp_node, void* value, Manager* manager) {
     //if there's only one HashNode
     if(temp_node->next == NULL){
         HashNode* new_node = malloc(sizeof(HashNode));
-        temp_node->prev = new_node;
+        temp_node->prev = (struct HashNode *) new_node;
         new_node->prev = NULL;
-        new_node->next = temp_node;
+        new_node->next = (struct HashNode *) temp_node;
         new_node->value_head = malloc(sizeof(ValueNode));
         new_node->value_head->next = NULL;
         new_node->value_head->value = value;
 
     }
+    //if temp node is head
+    else if(temp_node->prev == NULL){
+        HashNode* new_node = malloc(sizeof(HashNode));
+        temp_node->prev = (struct HashNode *) new_node;
+        new_node->next = (struct HashNode *) temp_node;
+        new_node->prev = NULL;
+        new_node->value_head = malloc(sizeof(ValueNode));
+        new_node->value_head->next = NULL;
+        new_node->value_head->value = value;
+        manager->head = new_node;
+    }
     else{
         HashNode* new_node = malloc(sizeof(HashNode));
-        HashNode* prev_node = temp_node->prev;
-        prev_node->next = new_node;
-        new_node->prev = prev_node;
-        new_node->next = temp_node;
-        temp_node->prev = new_node;
+        HashNode* prev_node = (HashNode *) temp_node->prev;
+        prev_node->next = (struct HashNode *) new_node;
+        new_node->prev = (struct HashNode *) prev_node;
+        new_node->next = (struct HashNode *) temp_node;
+        temp_node->prev = (struct HashNode *) new_node;
         new_node->value_head = malloc(sizeof(ValueNode));
         new_node->value_head->next = NULL;
         new_node->value_head->value = value;
@@ -42,8 +53,8 @@ void add_new_hash_node_in_the_middle(HashNode* temp_node, void* value) {
 
 void add_new_hash_node_at_the_end(HashNode* temp_node, void* value){
     HashNode* new_node = malloc(sizeof(HashNode));
-    temp_node->next = new_node;
-    new_node->prev = temp_node;
+    temp_node->next = (struct HashNode *) new_node;
+    new_node->prev = (struct HashNode *) temp_node;
     new_node->next = NULL;
     new_node->value_head = malloc(sizeof(ValueNode));
     new_node->value_head->next = NULL;
@@ -62,7 +73,16 @@ int check_hash_order(Manager* manager, void* value, HashNode* temp_node){
 
 
 void* get(Manager* manager, void* hash_value){
-
+    HashNode* temp_node = manager->head;
+    while(temp_node != NULL){
+        void* value_hash_ptr = manager->f_hash(temp_node->value_head->value);
+        int f_comp_result = manager->f_comp(hash_value, value_hash_ptr);
+        free(value_hash_ptr);
+        if(f_comp_result == 0){
+            return temp_node->value_head->value;
+        }
+        temp_node = (HashNode *) temp_node->next;
+    }
     return NULL;
 }
 
@@ -90,10 +110,10 @@ void insert(Manager* manager , void* value){
            }
            //if current hash value is greater
            else if(f_comp_result == 1){
-               add_new_hash_node_in_the_middle(temp_node, value);
+               add_new_hash_node_in_the_middle(temp_node, value,manager);
                return;
            }
-           temp_node = temp_node->next;
+           temp_node = (HashNode *) temp_node->next;
        }
        //if there's only one hash node
        if(temp_node->next == NULL && temp_node->prev == NULL){
@@ -103,7 +123,7 @@ void insert(Manager* manager , void* value){
             return;
           }
           else if(f_comp_result == 1){
-            add_new_hash_node_in_the_middle(temp_node, value);
+            add_new_hash_node_in_the_middle(temp_node, value,manager);
             return;
           }
           else if(f_comp_result == -1){
@@ -118,14 +138,13 @@ void insert(Manager* manager , void* value){
            return;
        }
        else if(f_comp_result == 1){
-           add_new_hash_node_in_the_middle(temp_node, value);
+           add_new_hash_node_in_the_middle(temp_node, value,manager);
            return;
        }
        add_new_hash_node_at_the_end(temp_node, value);
        return;
    }
 }
-
 
 void delete(Manager* manager, void* hash_value){
 
@@ -135,14 +154,13 @@ void clear(Manager* manager){
     HashNode* temp_node = manager->head;
     while(temp_node != NULL){
         ValueNode* temp_value_node = temp_node->value_head;
-        while(temp_value_node->next != NULL){
-            ValueNode* next_value_node = temp_value_node->next;
+        while(temp_value_node!= NULL){
+            ValueNode* next_value_node = (ValueNode *) temp_value_node->next;
             free(temp_value_node);
             temp_value_node = next_value_node;
         }
-        HashNode* next_hash_node = temp_node->next;
+        HashNode* next_hash_node = (HashNode *) temp_node->next;
         free(temp_node);
         temp_node = next_hash_node;
     }
-
 }
--- Rozproszone_lab/Rozproszone_lab1/main.c
+++ Rozproszone_lab/Rozproszone_lab1/main.c
@@ -3,19 +3,20 @@
 #include "hash_table.h"
 
 void print(Manager* manager){
-
     HashNode* temp_node = manager->head;
+    printf("Actual state of hashtable:\n");
     while(temp_node != NULL){
         void* value_hash_ptr = manager->f_hash(temp_node->value_head->value);
+        //printf("HashNode size: %zu bytes\n", sizeof(HashNode));
         printf("HashNode[%d]: ", *(int*)value_hash_ptr);
         free(value_hash_ptr);
         ValueNode* temp_value_node = temp_node->value_head;
         while(temp_value_node->next != NULL){
             printf("%d -> ", *((int*)temp_value_node->value));
-            temp_value_node = temp_value_node->next;
+            temp_value_node = (ValueNode *) temp_value_node->next;
         }
         printf("%d\n", *((int*)temp_value_node->value));
-        temp_node = temp_node->next;
+        temp_node = (HashNode *) temp_node->next;
     }
 
 }
@@ -39,24 +40,42 @@ void* f_hash(void* value) {
 
 
 int main() {
-    printf("Hello, World!\n");
-
     int (*f_comp_ptr)(void*, void*) = &f_comp;
     void (*print_ptr)(Manager*) = &print;
     void* (*f_hash_pointer)(void*) = &f_hash;
 
     Manager manager = {f_hash_pointer, f_comp_ptr, print_ptr, NULL};
 
-
     int x = 4;
     int y = 5;
     int z = 6;
+    int q = 8;
     int x1 = 11;
+    int q1 = 12;
+    int b = 7;
+
+    //showing insert function
     insert(&manager, (void *) &x);
     insert(&manager, (void *) &y);
     insert(&manager, (void *) &z);
     insert(&manager, (void *) &x1);
+    insert(&manager, (void *) &q);
+    insert(&manager, (void *) &q1);
+    insert(&manager, (void *) &b);
+
+    //showing get function
+    int index = 5;
+    int index1 = 1;
+    void* value = get(&manager,&index);
+    printf("Value is: %d\n", *(int*)value);
+    void* value2 = get(&manager,&index1);
+    printf("Value is: %d\n", *(int*)value2);
+
+
+    //printing actual state
+    printf("\n");
     print(&manager);
     clear(&manager);
+
     return 0;
 }
