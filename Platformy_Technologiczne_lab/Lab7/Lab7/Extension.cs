20cc2a826362f3e3cfff85957ff6c59c4a3d125f
Szymon Groszkowski <szymongr14@gmail.com>
2024-03-31 23:42:48 +0200

    started lab7 Platformy

---
+++ Platformy_Technologiczne_lab/Lab7/Lab7/Extension.cs
@@ -0,0 +1,56 @@
+namespace Lab7
+{
+    public static class MyExtensions
+    {
+        private static DateTime oldestDate=DateTime.MaxValue;
+
+        private static void GetOldestElementDateBody(this DirectoryInfo directoryInfo)
+        {
+            foreach (var d in directoryInfo.GetDirectories())
+            {
+                foreach (var f in directoryInfo.GetFiles())
+                {
+                    if (f.CreationTime < oldestDate)
+                    {
+                        oldestDate = f.CreationTime;
+                    }
+                }
+                GetOldestElementDateBody(d);
+            }
+        }
+
+        public static DateTime GetOldestElementDate(this DirectoryInfo directoryInfo)
+        {
+            GetOldestElementDateBody(directoryInfo);
+            return oldestDate;
+        }
+
+        public static string GetRAHS(this FileInfo fileInfo)
+        {
+            var rahsString = "";
+            if (fileInfo.IsReadOnly)
+            {
+                rahsString += 'r';
+            }
+            else rahsString += '_';
+            if (fileInfo.Attributes.HasFlag(FileAttributes.Archive))
+            {
+                rahsString += 'a';
+            }
+            else rahsString += '_';
+            if (fileInfo.Attributes.HasFlag(FileAttributes.Hidden))
+            {
+                rahsString += 'h';
+            }
+            else rahsString += '_';
+            if (fileInfo.Attributes.HasFlag(FileAttributes.System))
+            {
+                rahsString += 's';
+            }
+            else rahsString += '_';
+
+
+            return rahsString;
+        }
+    }
+}
---
+++ Platformy_Technologiczne_lab/Lab7/Lab7/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Mail;
+
+namespace Lab7
+{
+    class Program
+    {
+        private static int depth = 0;
+        public static void Main(string[] args)
+        {
+            var path = args[0];
+            var dirs = Directory.GetFiles(path);
+            
+            RecursiveSearch(path);
+            depth = 0;
+            var dirInfo = new DirectoryInfo(path);
+            var oldestDate = dirInfo.GetOldestElementDate();
+            Console.WriteLine(oldestDate);
+
+        }
+
+        private static void RecursiveSearch(string currentDir)
+        {
+            foreach (var directory in Directory.GetDirectories(currentDir))
+            {
+                for (var i = 0; i < depth * 4; i++)
+                {
+                    Console.Write(" ");
+                }
+                var dirInfo = new FileInfo(directory!);
+                var dirRahs = dirInfo.GetRAHS();
+                var dir = new DirectoryInfo(directory);
+                Console.Write(dir.Name + " ");
+                Console.WriteLine(dirRahs);
+                depth++;
+                var files = Directory.GetFiles(directory);
+                    // .Select(Path.GetFileName).ToArray();
+                foreach (var file in files)
+                {
+                    for (var i = 0; i < depth * 4; i++)
+                    {
+                        Console.Write(" ");
+                    }
+                    var fileInfo = new FileInfo(file!);
+                    var fileInfoName = fileInfo.Name;
+                    var rahs = fileInfo.GetRAHS();
+
+                    Console.Write(fileInfoName + " ");
+                    Console.WriteLine(rahs);
+                }
+                RecursiveSearch(directory);
+                depth--;
+            }
+        }
+    }
+}
