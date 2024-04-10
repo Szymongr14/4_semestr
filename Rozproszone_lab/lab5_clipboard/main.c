6fb08ec411951d755bac7cbe9e3a0cfad43fb3b7
Szymongr14 <szymongr14@gmail.com>
2024-04-10 19:21:49 +0200

    smalle changes in lab5_clipboard Rozproszone

--- Rozproszone_lab/lab5_clipboard/main.c
+++ Rozproszone_lab/lab5_clipboard/main.c
@@ -5,29 +5,28 @@
 int main()
 {
     while(1) {
-
         if (!OpenClipboard(NULL));
 
-        HANDLE hData = GetClipboardData(CF_TEXT);
+        HANDLE hData = GetClipboardData(CF_TEXT);//pobranie zawartosci schowka do uchwytu jakiegos
 
-        char *intercept_text = (char *) GlobalLock(hData); //zcastowanie locka na char pointer
+        char *intercept_text = (char *) GlobalLock(hData); //cast locka zablokowanej pamieci na heapie na char pointer
         printf("Original clipboard data: %s\n", intercept_text);
 
-        if (strlen(intercept_text) == 26 && strspn(intercept_text, "0123456789") == 26) {
-            printf("detected");
+        if (strlen(intercept_text) == 28 && strspn(intercept_text, "PL0123456789") == 28) {
+            printf("detected\n");
 
-            const char *newClipboardData = "DON'T PASTE!!!!!!";
+            const char *newClipboardData = "DON'T PASTE!!!!!!"; //nowy tekst jak wykryje numer konta
             size_t len = strlen(newClipboardData) + 1;
-            HANDLE hNewData = GlobalAlloc(GMEM_MOVEABLE, len);
-            char *new_string_pointer = (char *) GlobalLock(hNewData); //zcastowanie locka na char pointer
-            strcpy(new_string_pointer, newClipboardData);//skopiowanie nowego tekstu do globalnego locka
+            HANDLE hNewData = GlobalAlloc(GMEM_MOVEABLE, len);//alokowanie pamieci o dlugosci len na globalnym heapie
+            char *new_string_pointer = (char *) GlobalLock(hNewData); //cast locka na char pointer
+            strcpy(new_string_pointer, newClipboardData);//skopiowanie nowego tekstu pod adres globalnego locka
 
-            EmptyClipboard();
+            EmptyClipboard();//trzeba wyczyscic przed nadpisaniem, bo clipboard moze miec tylko jedna wartos tego samego typu
 
             if (SetClipboardData(CF_TEXT, hNewData) == NULL);
-            GlobalUnlock(hNewData);
+            GlobalUnlock(hNewData);//zwolnienie pamieci zaalokowanej na przekazanie tekstu do clipboard
         } else {
-            printf("not detected");
+            printf("not detected\n");
         }
 
         GlobalUnlock(hData);
