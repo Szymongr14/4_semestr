8017d83e3a34c0c660576840a959bd4d81d368c6
Szymongr14 <szymongr14@gmail.com>
2024-05-13 16:51:18 +0200

    changes in lab with pipes

--- Rozproszone_lab/lab7_pipes/main.c
+++ Rozproszone_lab/lab7_pipes/main.c
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include<sys/wait.h>
 
 #define READ 0
 #define WRITE 1
@@ -14,36 +15,49 @@ void send_string(const int* file_descriptor, const char* text)
 {
     close(file_descriptor[READ]);
     write(file_descriptor[WRITE], text, sizeof(text));
+    printf("p1: %s\n", text);
     close(file_descriptor[WRITE]);
 }
 
-void invert_letters(const int* file_descriptor)
+void invert_letters(const int* fd_1, const int* fd_2)
 {
-    char text[50];
-    read(file_descriptor[READ], text, sizeof(text));
-    close(file_descriptor[READ]);
+    close(fd_1[WRITE]);
+    close(fd_2[READ]);
 
-    for(int i=0; i<strlen(text);i++)
+    char text[50];
+    read(fd_1[READ], text, sizeof(text));
+    close(fd_1[READ]);
+    printf("before p2: %s\n", text);
+
+    if(strcmp(text, "exit") != 0)
     {
-        if((int)text[i] >= 65 && (int)text[i] <= 90)
+        for(int i=0; i<strlen(text);i++)
         {
-            text[i] = (char)((int)text[i] + 32);
-        }
-        else if((int)text[i] >= 97 && (int)text[i] <= 122)
-        {
-            text[i] = (char)((int)text[i] - 32);
+            if((int)text[i] >= 65 && (int)text[i] <= 90)
+            {
+                text[i] = (char)((int)text[i] + 32);
+            }
+            else if((int)text[i] >= 97 && (int)text[i] <= 122)
+            {
+                text[i] = (char)((int)text[i] - 32);
+            }
         }
     }
-    write(file_descriptor[WRITE], text, sizeof(text));
-    close(file_descriptor[WRITE]);
+    write(fd_2[WRITE], text, sizeof(text));
+    printf("p2: %s\n", text);
+    close(fd_2[WRITE]);
+    if(strcmp(text, "exit") == 0)
+    {
+        exit(0);
+    }
 }
 
-void multiply_digits(const int* file_descriptor)
+void multiply_digits(const int* fd_2)
 {
     char text[50];
-    close(file_descriptor[WRITE]);
-    read(file_descriptor[READ], text, sizeof(text));
-    close(file_descriptor[READ]);
+    close(fd_2[WRITE]);
+    read(fd_2[READ], text, sizeof(text));
+    close(fd_2[READ]);
 
     for(int i = 0;i<strlen(text); i++)
     {
@@ -62,14 +76,22 @@ void multiply_digits(const int* file_descriptor)
 
     const int fd = open("mainfifo", O_WRONLY);
     write(fd, text, sizeof(text));
+    printf("p3: %s\n", text);
     close(fd);
+    if (strcmp(text, "exit") == 0)
+    {
+        exit(0);
+    }
 }
 
 
 int main() {
-    int fd[2];
-    pipe(fd);
-    pid_t pid = fork();
+    int fd_1[2];
+    pipe(fd_1);
+    printf("fd_1: %d %d\n", fd_1[0], fd_1[1]);
+    int fd_2[2];
+    pipe(fd_2);
+    printf("fd_2: %d %d\n", fd_2[0], fd_2[1]);
 
     if(mkfifo("mainfifo", 0777) == -1)
     {
@@ -78,28 +100,32 @@ int main() {
             fprintf(stderr, "Error while creating named pipe");
         }
     }
+    pid_t pid = fork();
 
     if(pid == 0)
     {
-        char buffer[50];
-        scanf("%s", buffer);
-        send_string(fd, buffer);
-        exit(0);
+        do
+        {
+            char buffer[50];
+            scanf("%s", buffer);
+            send_string(fd_1, buffer);
+            // printf("process1 - sends data\n");
+            if(strcmp(buffer,"exit") == 0) exit(0);
+        }while(1);
     }
 
     pid = fork();
-    if(pid == 0)
+    if(pid == 0 )
     {
-        invert_letters(fd);
-        exit(0);
+        while(1)
+        {
+            invert_letters(fd_1, fd_2);
+        }
     }
 
-    pid = fork();
-    if(pid == 0)
+    while(1)
     {
-        multiply_digits(fd);
-        exit(0);
+        multiply_digits(fd_2);
     }
 
-    return 0;
 }
--- Rozproszone_lab/lab7_pipes/receive_and_print.c
+++ Rozproszone_lab/lab7_pipes/receive_and_print.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
     char buff[50];
     const int fd = open("mainfifo", O_RDONLY);
     read(fd, buff, sizeof(buff));
+    if(strcmp(buff, "exit") == 0) return 0;
     printf("RECIVED TEXT: %s\n", buff);
 
     return 0;
