1eb7975d876cbeb183d7eab023fb617459fd2443
Szymon Groszkowski <szymongr14@gmail.com>
2024-05-16 07:30:51 +0200

    started Java multithreading

---
+++ Rozproszone_lab/lab9_java/lab9_java_multithreading/src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Main {
+    private static final int CONSUMER_COUNT = 5;
+    private static final int PRODUCER_COUNT = 2;
+
+    public static void main(String[] args) {
+        ConcurrentMagazine magazine = new ConcurrentMagazine(100);
+
+        ExecutorService consumers = Executors.newFixedThreadPool(CONSUMER_COUNT);
+        for(int i=0;i<CONSUMER_COUNT;i++){
+            consumers.submit(new Consumer(magazine));
+        }
+
+        ExecutorService producers = Executors.newFixedThreadPool(PRODUCER_COUNT);
+        for (int i = 0; i < PRODUCER_COUNT; i++) {
+            producers.submit(new Producer(magazine));
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if ("quit".equalsIgnoreCase(input)) {
+                break;
+            }
+        }
+
+        producers.close();
+        consumers.close();
+    }
+}
